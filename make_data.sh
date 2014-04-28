a9946dff9ca98b96a44cd90b4b0e0f6fa743e87d
Arunabha Nag <nag.arunabha@gmail.com>
2014-04-28 14:11:59 -0400

    script to create input data for analysis

---
+++ make_data.sh
@@ -0,0 +1,29 @@
+#!/bin/sh           
+
+sourcedir="../E99-Language-Learning"
+hasher="php func-hash.php -p $sourcedir"
+gitbranch="back-end-working"
+
+datadir="data"
+commits="$datadir/commits.txt"
+
+git_cmd="git --git-dir $sourcedir/.git --work-tree $sourcedir"
+
+
+
+
+rm $datadir/*
+
+#get all the commit hashes
+$git_cmd log -3 --pretty=format:"%h - %ad" > $commits
+
+#go through each commit, hash the functions
+for commit in `cat $commits | cut -d ' ' -f1`
+do 
+    $git_cmd checkout $commit
+	$hasher > "$datadir/hash_$commit.txt"
+done
+
+#go back to the HEAD
+$git_cmd checkout $gitbranch
+
